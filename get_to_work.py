4ef4630fddc46bb6cf002419077673ebdb005895
Ryan Foust <r.foust1@outlook.com>
2022-11-15 12:04:47 -0500

    Added imports and debug flag; JSON created with some data to work with for now, will add more when implementation is more complete

--- get_to_work.py
+++ get_to_work.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+from todoist_api_python.api import TodoistAPI
+from tkinter import *
+import json
+import sys
+import subprocess
+import webbrowser
+debug = False
 
-def main():
-    # main function
-    print("Stubbed main")
+
+def main(argv):
+    if debug:
+        print("Starting main")
+        print("args received:", argv)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
