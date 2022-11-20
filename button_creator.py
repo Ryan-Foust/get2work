dc7860c86a7b6634a367579a5cc839bb541031a0
Ryan Foust <r.foust1@outlook.com>
2022-11-20 00:13:41 -0500

    Completed custom class, GUI now opens apps and websites after exit

--- button_creator.py
+++ button_creator.py
@@ -1,6 +1,27 @@
 # this package is to handle creating each of the Checkbutton objects
+from tkinter import *
+
+stage = Tk()
 class createCheckbutton:
+    # initiate class fields
     def __init__(self, app_data):
-        pad_X = 15
-        pad_Y = 15
-        apps = app_data
+        self.pad_X = 15
+        self.pad_Y = 15
+        self.apps = app_data
+        self.ctrl = IntVar()
+        self.button = self.make_button()
+
+    # how is the app_data parameter being presented?
+    def debug_func(self):
+        print(self.apps)
+
+    def _func(self):
+        print("demo function triggered")
+        print(self.ctrl.get())
+
+    # for each app, return a functioning button
+    def make_button(self):
+        return Checkbutton(stage, text=self.apps['name'], variable=self.ctrl, command=self._func)
+
+    def start_GUI(self):
+        stage.mainloop()
--- get_to_work.py
+++ get_to_work.py
@@ -6,33 +6,60 @@ import sys
 import subprocess
 import webbrowser
 import button_creator
-debug = True
+debug = False
 
+def open_website(url):
+    if debug:
+        print("open_website called")
+        print("url passed:", url)
+    else:
+        webbrowser.open(url)
+
+def open_app(path):
+    if debug:
+        print("open_app called")
+        print("path passed:", path)
+    else:
+        subprocess.Popen(path)
 
 def main(argv):
     if debug:
         print("Starting main")
         print("args received:", argv)
 
-        # grab token to access task manager app
-        with open(argv[1]) as TDI_Token:
-            token = TDI_Token.read()
-            api = TodoistAPI(token)
+    # grab token to access task manager app
+    # with open(argv[1]) as TDI_Token:
+    #     token = TDI_Token.read()
+    #     api = TodoistAPI(token)
 
-        # open and convert json to python types
-        a_n_w_temp = open(argv[2])
-        to_load = a_n_w_temp.read()
-        apps_and_websites = json.loads(to_load)
+    # open and convert json to python types
+    a_n_w_temp = open(argv[1])
+    to_load = a_n_w_temp.read()
+    apps_and_websites = json.loads(to_load)
 
-        # check that json data made it
-        if debug:
-            print(apps_and_websites)
-            print(type(apps_and_websites))
+    # check that json data made it
+    if debug:
+        print(apps_and_websites)
+        print(type(apps_and_websites))
 
-        for app in apps_and_websites:
-            if debug:
-                print(app)
-            button_creator.createCheckbutton(app_data=app)
+    maker = None
+    obj_list = []
+    # all JSON stuff is passed to class
+    for app in apps_and_websites:
+        maker = button_creator.createCheckbutton(app_data=app)
+        obj_list.append(maker)
+        maker.button.pack()
+    maker.start_GUI()
+
+    # for each button made
+    for obj in obj_list:
+
+        # if box was checked (meaning, open app or website)
+        if obj.ctrl.get() == 1:
+            if obj.apps['is_website']:
+                open_website(obj.apps['url'])
+            else:
+                open_app(obj.apps['path'])
 
 
 if __name__ == '__main__':
